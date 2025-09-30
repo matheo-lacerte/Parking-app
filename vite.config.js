ba82716ac59f4fed1ec50b18dc2343fb8c338fa6
Yukio <mlacerte2013@gmail.com>
2025-09-30 10:59:57 -0400

    fix: update import statement for package.json to use 'with' syntax

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' };
+
 
 export default defineConfig({
   define: {
